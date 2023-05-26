ae11efef3bb0b1293bac1d8c67bf7828db793452
logix <74055355+anyastrophic@users.noreply.github.com>
2023-05-26 23:26:42 +0300

    stuff

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,18 @@ module.exports = {
   ],
   theme: {
     extend: {
+      rotate: {
+        '360': '360deg',
+      },
+      dropShadow: {
+        glow: [
+          "0 0px 5pt rgba(255,255, 255, 0.2)",
+          "0 0px 10pt rgba(255, 255,255, 0.2)",
+        ],
+      },
+      colors: {
+        'black-transparent': 'rgba(9, 9, 11, 0.8)',
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':

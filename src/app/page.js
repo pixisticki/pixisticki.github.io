0fa055a6afb3e4f0794f6fbdd2ae9215f97ebc3f
pixi <haleysandy2715@gmail.com>
2023-07-15 17:46:13 -0700

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -73,7 +73,7 @@ export default function Home() {
             <div className={`w-[10rem] h-[0.05rem] ${styles._rainbowDivider}`}></div>
           </h2>
           <h3 className="self-center text-sm mt-4 text-zinc-400">
-            <a className="font-bold text-sm">“</a><i>i love hal</i><a className="font-bold text-xl">„</a><br/> - anya
+            <a className="font-bold text-lg">“</a><i>i love hal</i><a className="font-bold text-xl">„</a><br/> - anya
           </h3>
           <div className="flex self-center items-center mt-6">
             <img
